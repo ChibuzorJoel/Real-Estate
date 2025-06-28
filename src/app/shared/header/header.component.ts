d7d41e87b9cc06af0a8b7f763fa632edde73ca7e
ChibuzorJoel <Chibuzoronyedikachi72@gmail.com>
2025-06-28 22:35:16 +0100

    first commit

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
- menuOpen = false;
+  menuOpen = false;
+
+  constructor(private router: Router) {
+    // Optional: Auto-close menu on route change
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.menuOpen = false;
+      }
+    });
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 }
